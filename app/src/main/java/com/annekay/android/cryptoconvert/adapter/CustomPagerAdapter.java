e4f136fb2f0702f4349954498891afb5dc7a0289
Annekay <nwannauk@gmail.com>
2017-10-26 04:44:31 +0100

    Uses ViewPager to display fragments

---
+++ app/src/main/java/com/annekay/android/cryptoconvert/adapter/CustomPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.annekay.android.cryptoconvert.adapter;
+
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import com.annekay.android.cryptoconvert.view.FeaturedCoinsFragment;
+import com.annekay.android.cryptoconvert.view.RecentFragment;
+
+import static android.net.wifi.p2p.nsd.WifiP2pServiceRequest.newInstance;
+
+/**
+ * Created by annekay on 10/26/2017.
+ */
+
+public class CustomPagerAdapter extends FragmentPagerAdapter {
+    private String tabTitles[] = new String[] { "Recent", "Featured Coins" };
+
+    public CustomPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new RecentFragment();
+        }else {
+            return new FeaturedCoinsFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
---
+++ app/src/main/java/com/annekay/android/cryptoconvert/view/MainActivity.java
@@ -0,0 +1,32 @@
+package com.annekay.android.cryptoconvert.view;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.annekay.android.cryptoconvert.R;
+import com.annekay.android.cryptoconvert.adapter.CustomPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        CustomPagerAdapter adapter = new CustomPagerAdapter(getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+}
