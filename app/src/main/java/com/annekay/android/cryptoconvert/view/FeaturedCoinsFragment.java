2c7e31bb7764c69ab9f3a980932c73b6a0685126
Annekay <nwannauk@gmail.com>
2017-11-02 15:27:25 +0100

    uses the swipeefreshlayout to reload values

--- app/src/main/java/com/annekay/android/cryptoconvert/view/FeaturedCoinsFragment.java
+++ app/src/main/java/com/annekay/android/cryptoconvert/view/FeaturedCoinsFragment.java
@@ -7,9 +7,12 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -20,6 +23,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,10 +45,15 @@ import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FeaturedCoinsFragment extends Fragment {
+public class FeaturedCoinsFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener{
     private RecyclerView mRecyclerView;
     private final String CRYPTO_URL= "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD,EUR,NGN,BRL,BWP,GHC,HKD,JPY,KPW,CNY,DKK&extraParams=cryptocon";
-    private ProgressDialog mDialog;
+
+    private TextView mErrorMessageDisplay;
+
+    private ProgressBar mLoadingIndicator;
+    private TextView feedBackView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
     private CurrencyAdapter mAdapter;
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
     CryptoApiService mApiService = null;
@@ -65,9 +74,16 @@ public class FeaturedCoinsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.featured_layout, container, false);
         mRecyclerView = rootView.findViewById(R.id.recyclerView);
-        initVolleyCallback();
-        mCryptoUtil = new CryptoUtil(mApiService,getContext());
-        mCryptoUtil.getDataVolley("GETCALL", CRYPTO_URL);
+        mErrorMessageDisplay = rootView.findViewById(R.id.tv_error_message_display);
+        feedBackView = rootView.findViewById(R.id.feed_back);
+        mSwipeRefreshLayout = rootView.findViewById(R.id.swipe_container);
+        //apply different colors to the swipe refresh layout
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.orange, R.color.green, R.color.blue);
+
+        mLoadingIndicator = rootView.findViewById(R.id.pb_loading_indicator);
+        mLoadingIndicator.setVisibility(View.VISIBLE);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        detectNetworkState();
         setupView();
 
         return rootView;
@@ -76,20 +92,55 @@ public class FeaturedCoinsFragment extends Fragment {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mLoadingIndicator.setVisibility(View.GONE);
+    }
+    @Override
+    public void onRefresh() {
+        detectNetworkState();
+
     }
 
     public void initVolleyCallback(){
+       showWeatherDataView();
         mApiService = new CryptoApiService() {
             @Override
             public void notifySuccess(List<Crypto> mCryptoValues) {
+                mLoadingIndicator.setVisibility(View.GONE);
                 cryptoValues = mCryptoValues;
                 mAdapter.addValues(cryptoValues);
+                mSwipeRefreshLayout.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (mSwipeRefreshLayout.isRefreshing()) {
+                            mSwipeRefreshLayout.setRefreshing(false);
+                        }
+                    }
+                });
+
             }
 
             @Override
             public void notifyError(String requestType,VolleyError error) {
+                showErrorMessage();
             }
         };
+    }
+    // method to detect network connectivity
+    public void detectNetworkState() {
+        ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        if (isConnected) {
+            initVolleyCallback();
+            mCryptoUtil = new CryptoUtil(mApiService,getContext());
+            mCryptoUtil.getDataVolley("GETCALL", CRYPTO_URL);
+        } else {
+
+          showNetworkMessage();
+
+        }
     }
 
     private void setupView() {
@@ -101,14 +152,21 @@ public class FeaturedCoinsFragment extends Fragment {
         mAdapter = new CurrencyAdapter(getLayoutInflater());
         mRecyclerView.setAdapter(mAdapter);
     }
-
-
-    public void setProgress(boolean flag) {
-        if (flag) {
-            mDialog.show();
-        } else {
-            mDialog.dismiss();
-        }
+    private void showWeatherDataView() {
+        mErrorMessageDisplay.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
     }
+    private void showErrorMessage() {
+        mRecyclerView.setVisibility(View.GONE);
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+    private void showNetworkMessage(){
+        mRecyclerView.setVisibility(View.GONE);
+        mLoadingIndicator.setVisibility(View.GONE);
+        feedBackView.setVisibility(View.VISIBLE);
+        feedBackView.setText(R.string.feedback);
+
+    }
+
 
 }
